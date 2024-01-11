58d2c01852106f38601c7e584478b8eebb3b140f
nmrndll <81620556+nmrndll@users.noreply.github.com>
2024-01-11 19:42:33 +0800

    update ai plugin

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  new Typewriter("#poem", {
-    strings: "this is a nice topic",
+function displayPoem(response) {
+      new Typewriter("#poem", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
+    function generatePoem(event) {
+  event.preventDefault();
+let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "t0a95c0efc70cab947746c265ob4d438";
+  let prompt = "Generate a poem about ${instructionsInput}";
+  let context = "You are a poem expert and love to write witty short poems. Your misson is to write a 5 line poem in basic HTML";
+  let apiUrl = "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}";
+
+  axios.get(apiUrl).then(displayPoem)};
 
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
