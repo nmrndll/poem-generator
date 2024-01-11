36f191ca324255c79201b7af4b5ecf93cc45964f
nmrndll <81620556+nmrndll@users.noreply.github.com>
2024-01-11 20:00:06 +0800

    update user

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 function displayPoem(response) {
-      new Typewriter("#poem", {
+  new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
-    function generatePoem(event) {
-  event.preventDefault();
-let instructionsInput = document.querySelector("#user-instructions");
-  let apiKey = "t0a95c0efc70cab947746c265ob4d438";
-  let prompt = "Generate a poem about ${instructionsInput}";
-  let context = "You are a poem expert and love to write witty short poems. Your misson is to write a 5 line poem in basic HTML";
-  let apiUrl = "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}";
 
-  axios.get(apiUrl).then(displayPoem)};
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "t0a95c0efc70cab947746c265ob4d438";
+  let context =
+    "You are a poem expert and love to write witty poems. You mission is to generate a 5 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a short poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
+}
 
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
